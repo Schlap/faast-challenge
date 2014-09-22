f6e502c97a21f29fb64815b053281506131c6965
Hadi <hadichalabi@gmail.com>
2014-09-22 17:30:47 +0100

    Train has 0 passengers

--- lib/train.rb
+++ lib/train.rb
@@ -1,18 +1,6 @@
 class Train
 
 def passenger_count
-	@passenger_count = 0
+	0
+	end
 end
-
-def coaches(passenger)
-	@coaches = [[],[],[],[]]
-end
-
-def alight(passenger)
-	@coaches
-end
-end
-
-#def late!
-		#@late = true
-	#end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -1,25 +1,10 @@
 require 'train'
-require 'passenger'
 
 describe Train do
-	
-	it "should be empty after we create it" do
-		train = Train.new
-		expect(train.passenger_count).to eq(0)
-	end
 
-	it "should have 4 coaches after we initialize it" do
-		passenger = Passenger.new
-		train = Train.new 
-		expect(train.coaches(passenger)).to eq([[],[],[],[]])
-	end
+let(:train){Train.new}
 
-	it "should be able to alight passengers" do 
-		train = Train.new
-		passenger = Passenger.new
-		train.alight(passenger)
-		expect(train.passenger_count).to eq(0)
+it "should be empty after we initialize it" do
+	expect(train.passenger_count).to eq(0)
 	end
 end
-
-
